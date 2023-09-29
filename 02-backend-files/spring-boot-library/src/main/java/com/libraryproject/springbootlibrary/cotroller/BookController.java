e1cfe12f8cb36264c83b06e89619c70e1c57f672
Divyansh <bhardwajdivyansh234@gamil.com>
2023-09-29 23:12:28 +0530

    first commit

---
+++ 02-backend-files/spring-boot-library/src/main/java/com/libraryproject/springbootlibrary/cotroller/BookController.java
@@ -0,0 +1,41 @@
+package com.libraryproject.springbootlibrary.cotroller;
+
+import com.libraryproject.springbootlibrary.entity.Book;
+import com.libraryproject.springbootlibrary.service.BookService;
+import com.libraryproject.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/secure/currentloans/count")
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoansCount(userEmail);
+    }
+
+
+    @GetMapping("/secure/ischeckedout/byuser")
+    public boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
+                                      @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.checkoutBookByUser(userEmail, bookId);
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook (@RequestHeader(value = "Authorization") String token,
+                              @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+}
